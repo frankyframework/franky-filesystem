09adb0f9e0414a707ed561cade5c8f86f0d46d42
ussiel <ussiel@gmail.com>
2024-11-20 15:57:39 -0600

    8.3

--- src/Upload.php
+++ src/Upload.php
@@ -2714,7 +2714,7 @@ class Upload {
      */
     function getsize($size) {
         if ($size === null) return null;
-        $last = strtolower($size{strlen($size)-1});
+        $last = strtolower($size[strlen($size)-1]);
         $size = (int) $size;
         switch($last) {
             case 'g':
